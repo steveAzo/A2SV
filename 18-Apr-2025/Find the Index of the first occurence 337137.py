027668ae0e35957982895c956f6cb74ffd0fdc5c
Stephen Kwaku Azongo <135361078+steveAzo@users.noreply.github.com>
2025-04-18 21:51:39 +0000

    Created 'Find the Index of the first occurence' by A2SV Hub

---
+++ 18-Apr-2025/Find the Index of the first occurence 337137.py
@@ -0,0 +1,7 @@
+# Problem: Find the Index of the first occurence - https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        return haystack.find(needle)
+            
+        
