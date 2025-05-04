cb1f9dda71d7aac6508969b6e73489e5c541c109
Stephen Kwaku Azongo <135361078+steveAzo@users.noreply.github.com>
2025-05-04 13:43:00 +0000

    Created 'B - Ilya and Queries' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #9 04-May-2025/B - Ilya and Queries 348050.py
@@ -0,0 +1,21 @@
+# Problem: B - Ilya and Queries - https://codeforces.com/gym/606423/problem/B
+
+
+def queries(prefix, a, b):
+    return prefix[b - 1] - prefix[a - 1]
+
+def preprocess(s):
+    prefix = [0] * len(s)
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            prefix[i] = prefix[i - 1] + 1
+        else:
+            prefix[i] = prefix[i - 1]
+    return prefix
+
+s = input()
+prefix = preprocess(s)
+m = int(input())
+for _ in range(m):
+    l, r = map(int, input().split())
+    print(queries(prefix, l, r))
