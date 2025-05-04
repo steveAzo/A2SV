7f7481c1da03bab71ce9f86e2f6bd0144af5a8aa
Stephen Kwaku Azongo <135361078+steveAzo@users.noreply.github.com>
2025-05-04 13:42:34 +0000

    Created 'A - Promo' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #9 04-May-2025/A - Promo 348049.py
@@ -0,0 +1,15 @@
+# Problem: A - Promo - https://codeforces.com/gym/606423/problem/A
+
+
+n, q = map(int, input().split())
+p = list(map(int, input().split()))
+p.sort(reverse=True)
+prefix = [0] * (len(p) + 1)
+total = 0
+for j in range(len(p)):
+    total += p[j]
+    prefix[j] = total
+for _ in range(q):  
+    x, y = map(int, input().split())
+    res = prefix[x-1] - prefix[x-y-1]
+    print(res)
