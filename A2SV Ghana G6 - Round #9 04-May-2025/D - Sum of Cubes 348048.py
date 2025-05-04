d17de00f4cac7cd0203aded0c9de19869f3cb64b
Stephen Kwaku Azongo <135361078+steveAzo@users.noreply.github.com>
2025-05-04 13:42:22 +0000

    Created 'D - Sum of Cubes' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #9 04-May-2025/D - Sum of Cubes 348048.py
@@ -0,0 +1,17 @@
+# Problem: D - Sum of Cubes - https://codeforces.com/gym/606423/problem/D
+
+def isPossible(x):
+    for a in range(1, int(x**(1/3)) + 1):
+        if a**3 > x:
+            break
+        
+        diff = (x-a**3)
+        b = round(diff**(1/3))
+        if b**3 == diff and b > 0:
+            return "YES"
+    return "NO"
+
+t = int(input())
+for _ in range(t):
+    x = int(input())
+    print(isPossible(x))
