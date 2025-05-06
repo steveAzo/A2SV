aa0189dbb4275a0cc04540feea6063914027e746
Stephen Kwaku Azongo <135361078+steveAzo@users.noreply.github.com>
2025-05-06 00:10:01 +0000

    Created 'Permutations' by A2SV Hub

---
+++ 06-May-2025/Permutations 349038.py
@@ -0,0 +1,5 @@
+# Problem: Permutations - https://leetcode.com/problems/permutations/
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(itertools.permutations(nums))
