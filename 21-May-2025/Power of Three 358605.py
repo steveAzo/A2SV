e762ee29f517aa50fc34829d1b26a36205f43c8b
Stephen Kwaku Azongo <135361078+steveAzo@users.noreply.github.com>
2025-05-21 12:59:53 +0000

    Created 'Power of Three' by A2SV Hub

---
+++ 21-May-2025/Power of Three 358605.py
@@ -0,0 +1,14 @@
+# Problem: Power of Three - https://leetcode.com/problems/power-of-three/
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n == 1:
+            return True
+        if n == 0:
+            return False
+        
+        return self.isPowerOfThree(n/3)
+            
+        
+
+       
